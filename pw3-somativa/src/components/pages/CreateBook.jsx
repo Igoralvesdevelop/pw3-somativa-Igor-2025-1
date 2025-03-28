2ba0c88013f6cb14b83113a3936abdf90d0f51cd
igoralvesdevelop <igorisaias98@gmail.com>
2025-03-27 22:28:24 -0300

    Api Rest

--- pw3-somativa/src/components/pages/CreateBook.jsx
+++ pw3-somativa/src/components/pages/CreateBook.jsx
@@ -1,29 +1,49 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import style from './CreateBook.module.css'
 
 import Input from "../form/input";
 import Select from "../form/Select";
 import Button from "../form/Button";
+import { data } from "react-router-dom";
 const CreateBook = ()=>{
     /* Cria a Estrutura de state para dados de livros*/
     const [book, setBook] = useState({ });
+ /* Cria a Estrutura de state para dados de catwgoria*/
+    const [categories, setCategories] = useState({ });
 
-    /*Captura dos dados do eemento select */
+    /*Captura dos dados do elemento da input */
     function handlerChangeBook(event){
         setBook({...book, [event.target.name] : event.target.value});
         console.log(book);
     }
-    /*Captura dos dados do eemento select */
+    /*Captura dos dados do elemento select */
     function handlerChangeCategory(event){
-        setBook({...book, category : event.target.options[event.target.selectedIndex].text})
+        setBook({...book, cod_categoria : event.target.options[event.target.selectedIndex].text})
     }
     /* ENVIA OS DADOS PARA A API*/
     function submit(event){
         event.preventDefault();
         console.log(book);
     }
+    /* recupera os dados de categoria da apirest*/
+    useEffect(()=>{
+        fetch('https://127.0.0.1.5000/istagemCateorias', {
+            method: 'GET',
+            headers:{
+                'Content-Type':'',
+                'Access-Control-Allow-Origin':'*',
+                'Access-Control-Allow-Headers':'*'
+            }
 
+        }).then((response)=>
+            response.json()
+        ).then((categorias)=>{
+            console.log(categorias.data)
+        }).catch((error)=>{
+            console.log('ERRO: ' + error)
+        });
+    }, []);
     return(
         <section className={style.create_book_container}>
             <h1>CADASTRO DE LIVRO</h1>
@@ -31,30 +51,30 @@ const CreateBook = ()=>{
 
             <Input 
                 type='nome do livro'
-                name='txt_livro'
-                id='txt_livro'
+                name='nome_livro'
+                id='nome_livro'
                 placeholder='Digite o seu livro'
                 handlerChange={handlerChangeBook}
             />
             
             <Input 
                 type='autor do livro'
-                name='txt_autor'
-                id='txt_autor'
+                name='autor_livro'
+                id='autor_livro'
                 placeholder='Digite o nome do autor'
                 handlerChange={handlerChangeBook}
             />
             
             <Input 
                 type='descriçao do livro'
-                name='txt_descriçao'
-                id='txt_descriçao'
+                name='descricao_livro'
+                id='descricao_livro'
                 placeholder='Digite a descrição do livro'
                 handlerChange={handlerChangeBook}
             />
             <Select
-                name='slc_categoria'
-                id='slc_categoria'
+                name='cod_categoria'
+                id='cod_categoria'
                 text='Categoria do Livro'
                 handlerChange={handlerChangeCategory}
             
